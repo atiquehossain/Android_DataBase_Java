6ff4ccf4c010c8b5061ad4a155dd610a0c5f3c10
atique.hossain <atique.hossain@savethechildren.org>
2022-10-11 23:46:26 +0600

    ListUI

--- app/src/main/java/com/example/java_database_advance/MainActivity.java
+++ app/src/main/java/com/example/java_database_advance/MainActivity.java
@@ -11,45 +11,14 @@ import com.example.java_database_advance.database.DatabaseManager;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseHelper databaseHelper;
-    DatabaseManager databaseManager;
-    String sTotal, sDate, sProductName, sProductCost;
-    int total,cost;
-    EditText eTotal, eDate, eProductName, eProductCost;
-    FloatingActionButton mBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        databaseHelper = new DatabaseHelper(this);
-        databaseManager = new DatabaseManager(this);
 
-        eTotal = findViewById(R.id.total);
-        eDate = findViewById(R.id.date);
-        eProductName = findViewById(R.id.name);
-        eProductCost = findViewById(R.id.Cost);
-        mBtn = findViewById(R.id.mSaveMaster);
 
-        mBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sTotal = eTotal.getText().toString().trim();
-                sDate = eDate.getText().toString().trim();
-                sProductName = eProductName.getText().toString().trim();
-                sProductCost = eProductCost.getText().toString().trim();
 
-                total =Integer.parseInt(sTotal);
-                cost =Integer.parseInt(sProductCost);
-
-               boolean databaseCheck = databaseManager.insertProductData(sDate,total,sProductName,cost);
-
-                if(databaseCheck == true){
-                    Log.d("Atique", "Sucess: ");
-                }else
-                Log.d("Atique", "failed: ");
-
-            }
-        });
     }
 }
