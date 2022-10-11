a69defaa78b32fb6b3fb427d5a8e401db558c8f2
atique.hossain <atique.hossain@savethechildren.org>
2022-10-11 23:59:29 +0600

    ListUI

--- app/src/main/java/com/example/java_database_advance/ListActivity.java
+++ app/src/main/java/com/example/java_database_advance/ListActivity.java
@@ -2,13 +2,29 @@ package com.example.java_database_advance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ListActivity extends AppCompatActivity {
+    FloatingActionButton addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        addBtn = findViewById(R.id.mSaveMaster);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ListActivity.this,EntryActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
